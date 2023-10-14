b451fdf1950419d9a40f754f9146a44b3823c42e
ginosy <ginosy20@gmail.com>
2023-10-14 13:30:09 -0500

    tagline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>Browser Parking Lot</h1>
+                <p>Send most of your browser tabs into retirement.</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
